74a3740adfc76ad73384df79629f97566008f332
Sharan S poojari <117771260+Sharanpoojarissp@users.noreply.github.com>
2023-04-19 22:56:31 +0530

    Add files via upload

---
+++ N1-intro-nodejs.js
@@ -0,0 +1,54 @@
+<!-- c req s for website eg : google.com 
+s sends the ws in form of html , css and js , {inbrowser js }
+and the js in the backend is backend js -- which will be executed in js  -->
+ 
+
+<!-- what is nodejs actually ?  
+browser executes the js 
+scalable too - 1 se 1000 users at a time easy 
+open source 
+run js on server
+all rounder ( front end and backend too ) 
+supports multiple 
+
+many famouse companies use it 
+usefull inbuilt modules - importable codes  
+is fast in nature ! -->
+
+<!-- telusko - nodejs intro 
+in order for the browser to understand the langauage of js 
+it need to be converted into machine code using the js engine {chrome, mozilla firefox etcs   -- vs search enghin of chrome 
+bc it convert code to machine language }
+
+run time environment where we can run js -- instead of running it in browser 
+expresss js is the framework for developing the web applications 
+eg : java has spring and python has django to build web appln 
+
+after installing the nodejs and everthing said in the video 
+go to terminal and type -- node 
+
+now we can write the js code like we use to do in the console of browser 
+
+> const a = 100
+> console.log(a)
+op -- 100 
+
+now -- L63.js file made and write the code of hello world suing console log 
+
+and run it in terminal using run symbol present 
+
+if it dont run use node .\L63.js
+
+maza nhi aya bro -- nodejs has given us code in their ws in about us , just copy paste and run 
+
+op will be giving a https
+click on it and will retrun the op as helloworld - server bana diya   contratulations !! 
+
+
+
+
+
+
+
+
+ -->
